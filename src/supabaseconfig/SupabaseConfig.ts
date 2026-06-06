e5ffe792b91a7a571ab6ebf3ee540dc07536a3b5
Ronnie <ronnieroymwangi.com>
2026-06-06 22:51:55 +0300

    sign up complete

--- src/supabaseconfig/SupabaseConfig.ts
+++ src/supabaseconfig/SupabaseConfig.ts
@@ -2,6 +2,6 @@ import { createClient } from "@supabase/supabase-js";
 import type { Database } from "./supabase";
 
 export const supabase = createClient<Database>(
-  import.meta.env.VITE_SUPABASE_URL,
-  import.meta.env.VITE_SUPABASE_ANNON_KEY,
+  "https://benstmobfeaydjzujcvp.supabase.co",
+  "sb_publishable_w6QAuodru4KW8tkzJZCjmQ_OUsms3cG",
 );
